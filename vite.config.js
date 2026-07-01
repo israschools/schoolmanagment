eed50d2c3236bbbc1ae34fbf8df8f6f01a5e46d4
israschools <schoolsisra@gmail.com>
2026-07-01 21:38:12 +0500

    vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// GitHub Pages fix — repo name ke sath match karna zaroori hai
-export default defineConfig({
-  base: '/schoolmanagment/',   // ← aapka exact repo name
-  plugins: [react()],
-})
