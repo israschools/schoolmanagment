9979526385a63d0f758b8444a8253e2b30202362
israschools <schoolsisra@gmail.com>
2026-07-02 09:18:25 +0500

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@
 
 const APPS_SCRIPT_URL = "";
 // Example:
-// const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyp3mv2HeF2sPOnsWSIri271LeKs5HK-XxspXEiYaKIXQ26EAYh9-gXwYI5vfsNCey2/exec";
+// const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec";
 
 // ── Sheet names (Google Sheet mein same naam rakhen) ─────────
 export const SHEET_NAMES = {
